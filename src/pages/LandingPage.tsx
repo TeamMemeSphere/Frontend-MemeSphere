7db0027191a47548ab18a0a12c68a7cc3314c4b8
ruiiary <guidews39@gmail.com>
2025-01-07 00:51:01 +0900

    style: 색상, 폰트 GlobalStyle에 추가

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,6 +2,7 @@ const LandingPage = () => {
   return (
     <>
       <h3>LandingPage</h3>
+      안녕하세요.
     </>
   );
 };
--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,6 +1,7 @@
 // src/styles/GlobalStyle.js
 import { createGlobalStyle } from "styled-components";
 import "normalize.css";
+import "./fonts/font.css";
 
 // 변수명 앞에 두 개의 대시(--)를 붙여서 사용합니다.
 // :root 의사 클래스는 문서 트리의 루트 요소를 선택합니다. <html> 요소와 동일합니다.
@@ -15,7 +16,18 @@ const GlobalStyle = createGlobalStyle`
   :root {
     --font-size-md: 1.5rem;
     --font-size-base: 1.125rem;
-    --color-white: #ffffff; // 예시 
+
+    --color-blue: #345DFD; 
+    --color-purple: #7061F0; 
+    --color-pink: #DE8DFA; 
+
+    --color-green: #4FFC91; 
+    --color-yellow: #D6F84C;
+    --color-black: #161616;
+    --color-gray: #36363E;
+
+    --font-size-base: 1rem; /* 기본 폰트 크기 */
+    --font-family-base: "Pretendard"; /* Pretendard 폰트 */
   }
 
   /* 기본적인 HTML 및 Body 스타일 추가 */
@@ -25,6 +37,8 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     width: 100%;
     height: 100%;
+    font-family: var(--font-family-base);
+    font-size: var(--font-size-base);
   }
 
   body {
