ac99cba2844014893055b27acc4b9b4ee61f2c0a
sobinMoon <sobin021206@naver.com>
2025-01-09 16:49:51 +0900

    feature: 코인 카드 및 코인 리스트 구현

---
+++ src/components/Common/CoinList.tsx
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+import CoinCard from "./CoinCard";
+import { Coin } from "./CoinCard";
+
+const CoinList: React.FC<{ coins: Coin[] }> = ({ coins }) => {
+
+    return (
+        <Container>
+            <List>
+                {
+                    coins.map((coin, index) => {
+                        return (
+                            <CoinCard
+                                key={index}
+                                name={coin.name}
+                                symbol={coin.symbol}
+                                tradePrice={coin.tradePrice}
+                                highPrice={coin.highPrice}
+                                lowPrice={coin.lowPrice}
+                                change={coin.change}
+                                changePrice={coin.changePrice}
+                                changeRate={coin.changeRate}
+                                isCollected={coin.isCollected}
+                            />
+                        )
+                    })
+                }
+            </List>
+        </Container>
+    );
+
+}
+
+export default CoinList;
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+const List = styled.div`
+    display: grid;
+    /* grid-template-columns: repeat(3, 1fr); */
+    /* grid-template-columns: repeat(3, 340px); */
+    grid-template-columns: repeat(3, 340px);
+    width: fit-content;
+    /* width: 100%; */
+    /* width: min(75vw, 1440px); */
+    height: fit-content;
+    place-items: center;
+    /* gap: 39px clamp(30px, 1.563vw, 50px); */
+    gap: 39px 1.563vw;
+
+    @media (max-width: 1339px) {
+        grid-template-columns: repeat(2, 340px);
+    }
+
+    @media (max-width: 768px) {
+        grid-template-columns: repeat(1, 340px);
+    }
+`
