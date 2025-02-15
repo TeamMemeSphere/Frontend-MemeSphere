1ba3810f00b55920b95d595b8e4e0ba676971e0c
jjamming <ohj3694@naver.com>
2025-02-15 22:19:54 +0900

    feat: SSE 훅으로 구현

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -10,10 +10,8 @@ import AlarmModal from "../Modal/AlarmModal";
 import UserModal from "../Modal/Auth/UserModal";
 import { NavLink } from "react-router-dom";
 import LoginRequiredModal from "../Modal/LoginRequiredModal";
-import { EventSourcePolyfill } from "event-source-polyfill";
-import { API_ENDPOINTS } from "../../api/api";
-import { useAuth } from "../../hooks/common/useAuth";
 import {toast} from "react-toastify";
+import useSSEAlert from "../../hooks/common/useSSEAlert";
 
 const Navbar: React.FC = () => {
   const [isCompact, setIsCompact] = useState(window.innerWidth <= 1234);
@@ -22,12 +20,13 @@ const Navbar: React.FC = () => {
 
   const [isUserModalOpen, setIsUserModalOpen] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const {isAuthenticated} = useAuth();
+  
   const authTokens = {
     accessToken: localStorage.getItem("accessToken") ?? "",
     refreshToken: localStorage.getItem("refreshToken") ?? "",
   };
-  const eventSourceRef = useRef<EventSourcePolyfill | null>(null);
+  
+  useSSEAlert();
 
   useEffect(()=>{
     setTimeout(() => {
@@ -68,74 +67,6 @@ const Navbar: React.FC = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
-// 푸시 알림 수신을 위한 SSE 연결
-useEffect(() => { 
-  if (!isAuthenticated) {
-    if (eventSourceRef.current) {
-      console.log("로그아웃 감지: SSE 연결 해제");
-      eventSourceRef.current.close();
-      eventSourceRef.current = null;
-    }
-    return;
-  }
-
-  const subscribeSSE = () => {
-    if (!eventSourceRef.current && authTokens.accessToken) {
-      console.log("SSE 구독 시작");
-
-      eventSourceRef.current = new EventSourcePolyfill(
-        API_ENDPOINTS.SUBSCRIBE_SSE,
-        {
-          headers: { Authorization: `Bearer ${authTokens.accessToken}` },
-          withCredentials: true,
-        }
-      );
-
-      eventSourceRef.current.onopen = () => {
-        console.log("SSE 연결 성공");
-      };
-
-      eventSourceRef.current.onmessage = (event) => {
-        try {
-          console.log("event.data",event.data);
-          const parsedData = JSON.parse(event.data);
-          toast(`🔥 ${parsedData.name}, 변동성 ${parsedData.volatility}% 도달!`, {
-            position: "top-right",
-            autoClose: 5000,
-            hideProgressBar: true,
-            closeOnClick: false,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "light",
-          });
-        } catch (error) {
-          console.log("문자열 데이터 응답 : ", event.data);
-          console.log("알림 푸시 메시지 처리 중 발생 :", error);
-        }
-      };
-
-      eventSourceRef.current.onerror = (error) => {
-        console.log("SSE 에러: ", error);
-        eventSourceRef.current?.close();
-        eventSourceRef.current = null;
-
-        setTimeout(() => {
-          if (isAuthenticated) {
-            subscribeSSE();
-          }
-        }, 5000);
-      };
-    }
-  };
-
-  subscribeSSE();
-
-  return () => {
-    console.log("페이지 이동: SSE 유지됨");
-  };
-}, [isAuthenticated]);
-  
   // 프로필 버튼 클릭 시 사이드바 닫힘
   const handleOpenUserModal = () => {
     setIsUserModalOpen(true);
