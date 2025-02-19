25596757ad13195916ef617df621f6fd53fe6eb4
sobinMoon <sobin021206@naver.com>
2025-02-19 17:06:35 +0900

    fix: 코인 카드 리스트 최대 3열로 수정

--- src/pages/CoinCollection.tsx
+++ src/pages/CoinCollection.tsx
@@ -161,8 +161,11 @@ const Container = styled.div`
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
-  padding: 1.938rem 12.24vw 4.5rem 12.24vw;
-  width: 100%;
+  /* padding: 1.938rem 12.24vw 4.5rem 12.24vw; */
+  padding: 1.938rem 0 4.5rem 0;
+  /* width: 100%; */
+  width: min(75vw, 67.5rem);
+  margin: auto;
   height: fit-content;
   min-height: 100vh;
 `;
--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -142,9 +142,12 @@ const Container = styled.div`
   box-sizing: border-box;
   display: flex;
   flex-direction: column;
-  padding: 1.938rem 12.24vw 4.5rem 12.24vw;
-  width: 100%;
+  /* padding: 1.938rem 12.24vw 4.5rem 12.24vw; */
+  padding: 1.938rem 0 4.5rem 0;
+  /* width: 100%; */
+  width: min(75vw, 67.5rem);
   height: fit-content;
+  margin: auto;
   min-height: 100vh;
 `;
 const KeyWordWrapper = styled.div`
